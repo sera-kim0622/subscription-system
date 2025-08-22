cc1aed2421a69f2cf6d6474e6d706201003f1678
eshika.kim <eshika@naver.com>
2025-08-22 20:44:07 +0900

    feat():user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,11 @@
-export class User {}
+import { Entity, Column } from 'typeorm';
+import { CoreEntity } from 'src/common/core.entity';
+
+@Entity('user')
+export class User extends CoreEntity {
+  @Column({ type: 'varchar', length: 255, unique: true, name: 'email' })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255, name: 'password' })
+  password: string;
+}
