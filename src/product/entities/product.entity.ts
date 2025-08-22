4a1f729098abd118347750e2a8de95568fd5a6e2
eshika.kim <eshika@naver.com>
2025-08-22 21:00:59 +0900

    feat(): product entity

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,14 @@
-export class Product {}
+import { Entity, Column } from 'typeorm';
+import { CoreEntity } from 'src/common/core.entity';
+
+@Entity()
+export class Product extends CoreEntity {
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  type: string; // 예: MONTHLY, YEARLY
+
+  @Column({ type: 'int' })
+  price: number;
+}
