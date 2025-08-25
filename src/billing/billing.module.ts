be47197f6a89b9f8c7ada43a1cc97471ab816113
eshika.kim <eshika@naver.com>
2025-08-25 12:19:34 +0900

    feat(): generate payment module

--- src/billing/billing.module.ts
+++ src/billing/billing.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BillingService } from './billing.service';
 import { BillingController } from './billing.controller';
+import { PaymentController } from './payment/payment.controller';
+import { PaymentService } from './payment/payment.service';
 
 @Module({
-  controllers: [BillingController],
-  providers: [BillingService],
+  controllers: [BillingController, PaymentController],
+  providers: [BillingService, PaymentService],
 })
 export class BillingModule {}
