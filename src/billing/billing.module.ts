052eebfe1c6a31c7b8efc164ce7d306c8109857c
eshika.kim <eshika@naver.com>
2025-08-25 12:20:51 +0900

    feat(): generate subscription module

--- src/billing/billing.module.ts
+++ src/billing/billing.module.ts
@@ -3,9 +3,11 @@ import { BillingService } from './billing.service';
 import { BillingController } from './billing.controller';
 import { PaymentController } from './payment/payment.controller';
 import { PaymentService } from './payment/payment.service';
+import { SubscriptionController } from './subscription/subscription.controller';
+import { SubscriptionService } from './subscription/subscription.service';
 
 @Module({
-  controllers: [BillingController, PaymentController],
-  providers: [BillingService, PaymentService],
+  controllers: [BillingController, PaymentController, SubscriptionController],
+  providers: [BillingService, PaymentService, SubscriptionService],
 })
 export class BillingModule {}
