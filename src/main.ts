46f54350bb5f87a703b1362db9fe056c454a5577
sera-kim <sera.kim0622@gmail.com>
2025-09-03 21:09:33 +0900

    chore(): api문서 바로가기를 위한 로그추가

--- src/main.ts
+++ src/main.ts
@@ -15,5 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
   await app.listen(3000);
+  console.log('api docs : http://localhost:3000/api-docs');
 }
 bootstrap();
