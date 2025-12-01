b0e3308ddce0a716c4f12df6843925dfd58d047c
sera-kim <sera.kim0622@gmail.com>
2025-12-01 18:33:13 +0900

    fix(): test코드 작성 중 문제 발견 - error 타입 반환하는 방식 변경

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import {
   Injectable,
   ConflictException,
   UnauthorizedException,
+  HttpException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -16,12 +17,19 @@ export class UserService {
   ) {}
 
   async create(dto: CreateUserDto): Promise<User> {
-    const exists = await this.repo.findOne({ where: { email: dto.email } });
-    if (exists) throw new ConflictException('Email already in use');
+    try {
+      const exists = await this.repo.findOne({ where: { email: dto.email } });
+      if (exists) throw new ConflictException('Email already in use');
 
-    const hashed = await bcrypt.hash(dto.password, 12);
-    const user = this.repo.create({ email: dto.email, password: hashed });
-    return this.repo.save(user);
+      const hashed = await bcrypt.hash(dto.password, 12);
+      const user = this.repo.create({ email: dto.email, password: hashed });
+      return this.repo.save(user);
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new Error('회원가입 도중 알 수 없는 에러가 발생하였습니다.');
+    }
   }
 
   async findByEmail(email: string) {
