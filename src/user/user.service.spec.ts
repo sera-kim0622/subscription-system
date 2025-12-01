b868015eab9f0cc85ccbcef49bc3645ebf695e44
sera-kim <sera.kim0622@gmail.com>
2025-12-01 20:34:30 +0900

    fix(): 테스트코드-beforeEach 최상단에 배치

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -4,34 +4,44 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { ConflictException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 jest.mock('bcrypt', () => ({
   hash: jest.fn(),
 }));
 
-describe('회원가입', () => {
-  let service: UserService;
-  let userRepository;
+let service: UserService;
+let userRepository;
+let jwtService: JwtService;
 
-  beforeEach(async () => {
-    userRepository = {
-      findOne: jest.fn(),
-      create: jest.fn(),
-      save: jest.fn(),
-    };
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        UserService,
-        {
-          provide: getRepositoryToken(User),
-          useValue: userRepository,
+beforeEach(async () => {
+  userRepository = {
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+  const module: TestingModule = await Test.createTestingModule({
+    providers: [
+      UserService,
+      {
+        provide: getRepositoryToken(User),
+        useValue: userRepository,
+      },
+      {
+        provide: JwtService,
+        useValue: {
+          sign: jest.fn().mockReturnValue('mock_token'),
+          verify: jest.fn().mockReturnValue({ userId: 1 }),
         },
-      ],
-    }).compile();
+      },
+    ],
+  }).compile();
 
-    service = module.get<UserService>(UserService);
-  });
+  service = module.get<UserService>(UserService);
+  jwtService = module.get<JwtService>(JwtService);
+});
 
+describe('회원가입', () => {
   it('이미 존재하는 이메일이면 에러를 반환한다.', async () => {
     userRepository.findOne.mockResolvedValue({ id: 1 });
     await expect(
@@ -46,6 +56,7 @@ describe('회원가입', () => {
       service.create({ email: 'sera.kim@gmail.com', password: 'pass1234' }),
     ).rejects.toThrow(error);
   });
+
   it('회원가입 성공 시 가입한 유저의 정보를 반환한다.', async () => {
     userRepository.findOne.mockResolvedValue(undefined);
     userRepository.create.mockReturnValue({ email: 'sera.kim@gmail.com' });
@@ -67,4 +78,14 @@ describe('회원가입', () => {
       password: hash,
     });
   });
+});
+
+describe('가입한 회원(로그인)인지 확인하는 함수', () => {
+  it('존재하지 않는 회원일 경우 UnauthorizedException을 반환', async () => {});
+
+  it('비밀번호가 일치하지 않는 경우 UnauthorizedException을 반환', async () => {});
+
+  it('로그인 중 알 수 없는 에러가 발생했을 때 에러 반환', async () => {});
+
+  it('로그인에 성공하여 accessToken을 반환', async () => {});
 });
