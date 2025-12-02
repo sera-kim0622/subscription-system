2d3f0661cb966e965b14ab461a64dcb9714acd27
sera-kim <sera.kim0622@gmail.com>
2025-12-02 16:22:13 +0900

    fix(): 비밀번호 조회 제외

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,14 +1,16 @@
-import { Entity, Column, OneToMany } from 'typeorm';
+import { Entity, Column, OneToMany, BeforeInsert } from 'typeorm';
 import { CoreEntity } from 'src/common/core.entity';
 import { Subscription } from 'src/billing/subscription/entities/subscription.entity';
 import { Payment } from 'src/billing/payment/entities/payment.entity';
+
+import * as bcrypt from 'bcrypt';
 
 @Entity('user')
 export class User extends CoreEntity {
   @Column({ type: 'varchar', length: 255, unique: true, name: 'email' })
   email: string;
 
-  @Column({ type: 'varchar', length: 255, name: 'password' })
+  @Column({ type: 'varchar', length: 255, name: 'password', select: false })
   password: string;
 
   @OneToMany(() => Subscription, (subscription) => subscription.user)
@@ -16,4 +18,9 @@ export class User extends CoreEntity {
 
   @OneToMany(() => Payment, (p) => p.user)
   payments: Payment[];
+
+  @BeforeInsert()
+  private async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
 }
