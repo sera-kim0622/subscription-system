f59a327204b3713583d2958dc1124bd308e77ece
sera-kim <sera.kim0622@gmail.com>
2025-12-05 14:54:31 +0900

    feat(): swagger 예시 작성

--- src/billing/product/dto/create-product.dto.ts
+++ src/billing/product/dto/create-product.dto.ts
@@ -1,1 +1,17 @@
-export class CreateProductDto {}
+import { IsIn, IsInt, IsString } from 'class-validator';
+import { PeriodType } from '../../subscription/types';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateProductInputDto {
+  @ApiProperty({ example: 'BASIC', description: '상품명' })
+  @IsString()
+  name: string;
+
+  @ApiProperty({ example: 'MONTHLY', description: '구독 기간: 월/연만 가능' })
+  @IsIn(['MONTHLY', 'YEARLY'])
+  type: PeriodType;
+
+  @ApiProperty({ example: 15000, description: '가격, 숫자만 입력 가능' })
+  @IsInt()
+  price: number;
+}
