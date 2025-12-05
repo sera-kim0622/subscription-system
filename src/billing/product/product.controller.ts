747cda9f7160f20a5959c38b8b5e1dd916ad99c4
sera-kim <sera.kim0622@gmail.com>
2025-12-05 17:43:26 +0900

    feat(): 구독상품 생성 어드민 전용으로 적용

--- src/billing/product/product.controller.ts
+++ src/billing/product/product.controller.ts
@@ -1,9 +1,13 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { GetProductsDto } from './dto/get-products.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Product } from './entities/product.entity';
 import { CreateProductInputDto } from './dto/create-product.dto';
+import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
+import { RolesGuard } from '../../auth/roles.guard';
+import { Roles } from '../../auth/decorator/roles.decorator';
+import { UserRole } from '../../user/entities/user.entity';
 
 @ApiTags('products')
 @Controller('products')
@@ -20,6 +24,9 @@ export class ProductController {
   }
 
   @Post()
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
   async createProduct(@Body() body: CreateProductInputDto): Promise<Product> {
     return await this.productService.createProduct(body);
   }
