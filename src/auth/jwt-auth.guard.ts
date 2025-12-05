06684d568061fb9369b942184cfbd21c0a86eb40
sera-kim <sera.kim0622@gmail.com>
2025-12-05 17:42:40 +0900

    fix(): console.log삭제

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -4,7 +4,6 @@ import { AuthGuard } from '@nestjs/passport';
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   canActivate(context: ExecutionContext) {
-    console.log('🔥 JwtAuthGuard reached');
     return super.canActivate(context);
   }
 }
