b6e11e0c3296a0378abfc84e2411eb5fabefc036
sera-kim <sera.kim0622@gmail.com>
2025-12-08 20:00:04 +0900

    fix(): 서비스 결제 함수 전반적인 로직 수정

--- src/billing/payment/payment.service.ts
+++ src/billing/payment/payment.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { randomUUID } from 'crypto';
@@ -7,12 +12,18 @@ import { Product } from '../product/entities/product.entity';
 import { PurchaseInputDto, PurchaseOutputDto } from './dto/purchase.dto';
 import { PortOneResult } from './portone/portone.types';
 import { SubscriptionService } from '../subscription/subscription.service';
+import { Subscription } from '../subscription/entities/subscription.entity';
+import { Payment } from './entities/payment.entity';
+import { UserService } from '../../user/user.service';
+import { PAYMENT_STATUS } from './entities/payment.status';
 
 @Injectable()
 export class PaymentService {
   constructor(
     @InjectRepository(Product)
-    private readonly productRepo: Repository<Product>,
+    private readonly productRepository: Repository<Product>,
+    private readonly paymentRepository: Repository<Payment>,
+    private readonly userService: UserService,
     private readonly subscriptionService: SubscriptionService,
   ) {}
 
@@ -26,7 +37,7 @@ export class PaymentService {
     const { productId, simulate } = dto;
 
     // 1. 현재 판매중인 상품인지 확인
-    const product = await this.productRepo.findOne({
+    const product = await this.productRepository.findOne({
       where: { id: productId },
     });
 
@@ -34,44 +45,98 @@ export class PaymentService {
       throw new NotFoundException('존재하지 않는 상품입니다.');
     }
 
-    // ===== 가짜 결제 모델 만드는 로직 =====
+    // 2. 현재 유료 구독을 하고 있는지 확인
+    const user = await this.userService.getUser(userId);
+
+    if (!user) {
+      throw new UnauthorizedException('유저 정보가 확인되지 않습니다.');
+    }
+
+    const paidSubscription =
+      await this.subscriptionService.getCurrentSubscription(userId);
+
+    if (paidSubscription) {
+      throw new BadRequestException('이미 구독중인 상품이 있습니다.');
+    }
+
+    // ===== mock 결제 모델 만드는 로직 =====
     const isFail = simulate === 'fail';
     const pgPaymentId = randomUUID();
 
-    const result: PortOneResult = isFail
+    const pgPaymentResult: PortOneResult = isFail
       ? {
-          status: 'FAILED',
+          status: PAYMENT_STATUS.FAIL,
           failReason: 'SIMULATED_FAILURE',
         }
       : {
           pgPaymentId,
-          status: 'PAID',
+          status: PAYMENT_STATUS.SUCCESS,
           paidAt: new Date().toISOString(),
         };
+    // ===== mock 결제 모델 만드는 로직 =====
 
+    // 3. mock 결제 정보 payment 테이블에 저장
+    const paymentObject = this.paymentRepository.create({
+      user,
+      pgPaymentId: pgPaymentResult.pgPaymentId,
+      status: pgPaymentResult.status,
+      amount: product.price,
+      paymentDate: pgPaymentResult.paidAt ?? null,
+      issuedSubscription: false,
+    });
+
+    const result = pgPaymentResult;
+
+    let paymentResult: Payment;
+    try {
+      paymentResult = await this.paymentRepository.save(paymentObject);
+    } catch (error) {
+      throw new BadRequestException('결제 내역 저장에 실패했습니다.');
+    }
+
+    // 3. 결제 정보 받은 후 결과 반환
     // 결제 성공의 경우 : 구독 생성, 구독권과 결제 내역 반환
     // 결제 실패의 경우 : 결제 내역 반환
-    if (result.status === 'PAID') {
-      const subscription = await this.subscriptionService.createSubscription({
-        userId,
-        productId,
-        period: product.type,
-        paymentId: 1,
-      });
+    if (paymentResult.status === PAYMENT_STATUS.SUCCESS) {
+      let subscription: Subscription;
+      try {
+        subscription = await this.subscriptionService.createSubscription({
+          userId,
+          productId,
+          period: product.type,
+          paymentId: 1,
+        });
+      } catch (error) {
+        for await (const per of [1, 2, 3]) {
+          try {
+            subscription = await this.subscriptionService.createSubscription({
+              userId,
+              productId,
+              period: product.type,
+              paymentId: 1,
+            });
+          } catch (error) {
+            console.log(`${per}회 구독권 발급 시도 중`);
+            continue;
+          }
+        }
+      }
 
       return {
-        result,
         order: {
           productId: product.id,
           name: product.name,
           type: product.type,
           price: product.price,
         },
-        subscription,
+        payment: paymentResult,
+        subscription: subscription ?? null,
+        issuedSubscriptionResult: subscription
+          ? '결제 완료 후 구독권 발급에 성공하였습니다.'
+          : '결제는 성공하였으나 구독권 발급에 실패하였습니다.',
       };
-    } else {
+    } else if (paymentResult.status === PAYMENT_STATUS.FAIL) {
       return {
-        result,
         order: {
           productId: product.id,
           name: product.name,
