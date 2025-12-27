5b199ec48578879ebef8541b4d1231da24dfba8f
sera-kim <sera.kim0622@gmail.com>
2025-12-27 17:52:06 +0900

    fix(): async/await이 없는 부분에 대한 이유 주석 추가

--- src/billing/payment/payment.controller.ts
+++ src/billing/payment/payment.controller.ts
@@ -10,6 +10,9 @@ import { CurrentUser } from 'src/auth/decorator/current-user.decorator.ts';
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
+  // controller에서 추가 로직이 없기때문에 service는 promise를 리턴하고
+  // 리턴할 때까지 nestJS가 기다려주기 때문에 굳이 controller에서 await처리를
+  // 하지 않아도 된다.
   @ApiOperation({ summary: '구독 상품 구매 (PortOne 모킹)' })
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
@@ -25,5 +28,5 @@ export class PaymentController {
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
   @Post('refund') // 환불 과정은 취소한 행위에 대한 새로운 처리이므로 관용적으로 post를 주로 사용, 결제대행사들도 모두 post를 사용함
-  refund() {}
+  refund(@CurrentUser('id') userId: number) {}
 }
