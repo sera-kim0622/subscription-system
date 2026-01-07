354d96a7cc170f4263496cb7e8a0a3825072278b
sera-kim0622 <sera.kim0622@gmail.com>
2026-01-07 16:24:20 +0900

    feat(): 프론트 cors허용,  dto에러 설정

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,33 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config, {
     deepScanRoutes: true,
   });
+
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const fieldErrors = errors.reduce(
+          (acc, cur) => {
+            acc[cur.property] = Object.values(cur.constraints ?? {})[0];
+            return acc;
+          },
+          {} as Record<string, string>,
+        );
+
+        return new BadRequestException({
+          type: 'VALIDATION_ERROR',
+          errors: fieldErrors,
+        });
+      },
+    }),
+  );
+
   SwaggerModule.setup('api-docs', app, document);
   await app.listen(3000);
   console.log('api docs : http://localhost:3000/api-docs');
