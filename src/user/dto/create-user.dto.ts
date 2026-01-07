1b2dbcb845efb62afd4c7dcc7b8aeef757eae88d
sera-kim0622 <sera.kim0622@gmail.com>
2026-01-07 16:24:44 +0900

    fix(): 회원가입 시 dto 에러 메세지 상세히 표시

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -4,12 +4,12 @@ import { UserRole } from '../entities/user.entity';
 
 export class CreateUserDto {
   @ApiProperty({ example: 'sera@gmail.com' })
-  @IsEmail()
+  @IsEmail({}, { message: '이메일 형식이 올바르지 않습니다.' })
   email: string;
 
   @ApiProperty({ example: 'password123' })
   @IsString()
-  @MinLength(8)
+  @MinLength(8, { message: '비밀번호는 8자리 이상이어야 합니다.' })
   password: string;
 
   @ApiProperty({ enum: ['USER', 'ADMIN'] })
