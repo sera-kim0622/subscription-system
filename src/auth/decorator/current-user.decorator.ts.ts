3745514ba2718c00b794d790561a9e5bece465b9
sera-kim0622 <sera.kim0622@gmail.com>
2026-01-09 17:32:15 +0900

    fix(): 에러 유발 금지 - 애초에 userId만 반환하도록

--- src/auth/decorator/current-user.decorator.ts.ts
+++ src/auth/decorator/current-user.decorator.ts.ts
@@ -1,9 +1,17 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import {
+  createParamDecorator,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 export const CurrentUser = createParamDecorator(
   (data: string | undefined, ctx: ExecutionContext) => {
     const req = ctx.switchToHttp().getRequest();
-    const user = req.user; // JwtAuthGuard/Strategy가 채움
-    return data ? user?.[data] : user;
+    const userId = req.user.userId;
+    if (!userId) {
+      throw new UnauthorizedException('INVALID_TOKEN_PAYLOAD');
+    }
+
+    return userId;
   },
 );
