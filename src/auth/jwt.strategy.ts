59099a982da45d73f1fac6c7c6023ed7ee5f36a3
sera-kim0622 <sera.kim0622@gmail.com>
2026-01-09 17:31:45 +0900

    fix(): cookie parser로 extract

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,16 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UserRole } from '../user/entities/user.entity';
+import { Request } from 'express';
+
+const cookieExtractor = (req: Request): string | null => {
+  if (req && req.cookies) {
+    return req.cookies['accessToken'] || null;
+  }
+  return null;
+};
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([cookieExtractor]),
       secretOrKey: process.env.JWT_SECRET || 'dev_secret',
     });
   }
   async validate(payload: { sub: string; email: string; role: UserRole }) {
+    console.log('user', payload.email);
     return {
       userId: Number(payload.sub),
       email: payload.email,
