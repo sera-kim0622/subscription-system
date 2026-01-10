c00c8b55a55e67a73409f3888a0d3d30d2073539
sera-kim0622 <sera.kim0622@gmail.com>
2026-01-10 18:28:24 +0900

    feat(): 날짜를 yyyy-MM-dd형식으로 바꿔주는 데코레이터 작성

---
+++ src/common/decorators/date.format.ts
@@ -0,0 +1,17 @@
+import { Transform } from 'class-transformer';
+
+export const TransformDate = (format: 'date' | 'datetime' = 'date') => {
+  return Transform(({ value }) => {
+    if (!value) {
+      return null;
+    }
+
+    const date = new Date(value);
+
+    if (format === 'date') {
+      return date.toISOString().slice(0, 10);
+    }
+
+    return date.toISOString().replace('T', ' ').slice(0, 19);
+  });
+};
