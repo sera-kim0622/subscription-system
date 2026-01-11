baa6023853ca73ab0af1e84c290fa7811aae1d03
sera-kim0622 <sera.kim0622@gmail.com>
2026-01-11 23:55:38 +0900

    test(): date반환하는 데코레이터 테스트

---
+++ src/common/decorators/date.format.spec.ts
@@ -0,0 +1,46 @@
+import { instanceToPlain } from 'class-transformer';
+import { TransformDate } from './date.format';
+
+class TestDateDto {
+  @TransformDate('date')
+  testDate: Date;
+}
+
+class TestDateTimeDto {
+  @TransformDate('datetime')
+  testDate: Date;
+}
+
+describe('Date객체를 yyyy-MM-dd로 시작하는 string으로 반환하는 함수 테스트', () => {
+  it('format을 date로 선택하면 yyyy-MM-dd형식으로 반환', () => {
+    const dto = new TestDateDto();
+
+    dto.testDate = new Date('2026-02-10T09:00:00');
+
+    const plain = instanceToPlain(dto);
+
+    expect(plain.testDate).toBe('2026-02-10');
+    expect(typeof plain.testDate).toBe('string');
+  });
+
+  it('format을 datetime으로 선택하면 yyyy-MM-dd HH:mm:ss으로 반환한다.', () => {
+    const dto = new TestDateTimeDto();
+
+    dto.testDate = new Date('2026-02-10T09:00:00');
+
+    const plain = instanceToPlain(dto);
+
+    expect(plain.testDate).toBe('2026-02-10 00:00:00');
+    expect(typeof plain.testDate).toBe('string');
+  });
+
+  it('값이 들어오지 않았을 경우 null을 반환한다..', () => {
+    const dto = new TestDateTimeDto();
+
+    dto.testDate = null;
+
+    const plain = instanceToPlain(dto);
+
+    expect(plain.testDate).toBe(null);
+  });
+});
